dbc393e45da43898f68dc7b4ecb5eeb0fe54d7d2
JLH <janlhai@icloud.com>
2025-02-13 23:42:33 +0100

    anpassen des whats new screens

--- SwipeClean/SwipeCleanApp.swift
+++ SwipeClean/SwipeCleanApp.swift
@@ -17,8 +17,9 @@ struct SwipeCleanApp: App {
                 \.whatsNew,
                 WhatsNewEnvironment(
                     // Specify in which way the presented WhatsNew Versions are stored.
+                    // zum entwikeln kann man diesen Modus verwenden InMemoryWhatsNewVersionStore
                     // In default the `UserDefaultsWhatsNewVersionStore` is used.
-                    versionStore: InMemoryWhatsNewVersionStore(),
+                    versionStore: UserDefaultsWhatsNewVersionStore(),
                     // Pass a `WhatsNewCollectionProvider` or an array of WhatsNew instances
                     whatsNewCollection: self
                 )
