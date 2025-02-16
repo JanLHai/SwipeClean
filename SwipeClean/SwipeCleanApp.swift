46e2432b8a7f732dd0cbf3d89cf83e4b52f6bcd6
JLH <janlhai@icloud.com>
2025-02-16 20:46:28 +0100

    erweiterung der was ist neu seite

--- SwipeClean/SwipeCleanApp.swift
+++ SwipeClean/SwipeCleanApp.swift
@@ -19,7 +19,7 @@ struct SwipeCleanApp: App {
                     // Specify in which way the presented WhatsNew Versions are stored.
                     // zum entwikeln kann man diesen Modus verwenden InMemoryWhatsNewVersionStore
                     // In default the `UserDefaultsWhatsNewVersionStore` is used.
-                    versionStore: UserDefaultsWhatsNewVersionStore(),
+                    versionStore: InMemoryWhatsNewVersionStore(),
                     // Pass a `WhatsNewCollectionProvider` or an array of WhatsNew instances
                     whatsNewCollection: self
                 )
@@ -38,13 +38,13 @@ extension SwipeCleanApp: WhatsNewCollectionProvider {
             // The Version that relates to the features you want to showcase
             version: "1.1.0",
             // The title that is shown at the top
-            title: "Was ist neu in SwipeClean",
+            title: "Was ist neu in Swipe Clean v. 1.1.0",
             // The features you want to showcase
             features: [
                 WhatsNew.Feature(
-                    image: .init(systemName: "character.bubble"),
-                    title: "Englische Sprache",
-                    subtitle: "Wir unterstützen nun auch neben der Deutschen die Englische Sprache"
+                    image: .init(systemName: "icloud"),
+                    title: "iCloud Synchronisation",
+                    subtitle: "Es gibt nun die möglichkeit die Einstellungen und Daten über die Bilder mit iCloud zu synchronisieren"
                 ),
                 WhatsNew.Feature(
                     image: .init(systemName: "play.rectangle"),
@@ -55,6 +55,11 @@ extension SwipeCleanApp: WhatsNewCollectionProvider {
                     image: .init(systemName: "speaker.slash"),
                     title: "Lautsärke Stummschalten",
                     subtitle: "Sie können nun in den Einstellungen entscheiden ob Sie Töne stummschalten möchten"
+                ),
+                WhatsNew.Feature(
+                    image: .init(systemName: "character.bubble"),
+                    title: "Zusätzliche Sprachen",
+                    subtitle: "Wir unterstützen nun auch neben der Deutschen die Englische, Französch, Italienisch, Japanisch, Koreanisch, Portugiesisch, Hindi, Spanisch, Chinesisch (vereinfacht) und Russisch Sprache"
                 ),
             ],
             
